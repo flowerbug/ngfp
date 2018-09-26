6d2dbff215c067cfe4c9cebee757e4d04dc5d08c
ant <ant@anthive.com>
2018-09-26 10:10:28 -0400

    added white and widget active area lists

--- active.py
+++ active.py
@@ -1,5 +1,37 @@
 import pyglet
 
-def ActiveAreaAction (self, x, y):
+
+def ShowWhiteInArrow (self, x_rec, y_rec, win_pos):
     pass
+
+
+def ShowWhiteOutArrow (self, x_rec, y_rec, win_pos):
+    pass
+
+
+def MarbleInMotion (self, x_rec, y_rec, win_pos):
+    pass
+
+
+def DoWhiteAction (self, x_rec, y_rec, win_pos):
+
+    ShowWhiteInArrow (self, x_rec, y_rec, win_pos)
+    MarbleInMotion (self, x_rec, y_rec, win_pos)
+    ShowWhiteOutArrow (self, x_rec, y_rec, win_pos)
+
+
+def DoWidgetAction (self, x_rec, y_rec, win_pos):
+    pass
+
+
+def ActiveAreaAction (self, x_rec, y_rec, win_pos):
+
+    if (win_pos in self.white_active_squares):
+        print ("selected ", win_pos, " which is an active White square.")
+        DoWhiteAction (self, x_rec, y_rec, win_pos)
+    elif (win_pos in self.widget_active_squares):
+        print ("selected ", win_pos, " which is an active Widget square.")
+        DoWidgetAction (self, x_rec, y_rec, win_pos)
+    else:
+        pass
 
--- background.py
+++ background.py
@@ -14,20 +14,32 @@ def DrawBordersAndBackgrounds (self):
     self.fixed_sprites.append( pyglet.sprite.Sprite( self.blue_bg_image, batch=self.fixed_batch, group=self.background, x = x_pos_right, y = y_pos))
 
     # draw white game border
+    self.white_active_squares = []
+    self.white_active_squares_position = []
     y_pos = self.img_pix
     x_pos = 0
     x_pos_right = self.img_pix * (self.game_cols+1)
     for y in range(self.game_rows):
         self.fixed_sprites.append( pyglet.sprite.Sprite( self.white_bg_image, batch=self.fixed_batch, group=self.background, x = x_pos, y = y_pos))
+        self.white_active_squares.append((self.window_cols * (y_pos // self.img_pix)))
+        self.white_active_squares_position.append([x_pos,y_pos])
         self.fixed_sprites.append( pyglet.sprite.Sprite( self.white_bg_image, batch=self.fixed_batch, group=self.background, x = x_pos_right, y = y_pos))
+        self.white_active_squares.append((self.window_cols * (y_pos // self.img_pix))+self.game_cols+1)
+        self.white_active_squares_position.append([x_pos_right,y_pos])
         y_pos += self.img_pix
     y_pos = 0
     x_pos = self.img_pix
     y_pos_up = self.img_pix * (self.game_rows+1)
     for x in range(self.game_cols):
         self.fixed_sprites.append( pyglet.sprite.Sprite( self.white_bg_image, batch=self.fixed_batch, group=self.background, x = x_pos, y = y_pos))
+        self.white_active_squares.append(x+1)
+        self.white_active_squares_position.append([x_pos,y_pos])
         self.fixed_sprites.append( pyglet.sprite.Sprite( self.white_bg_image, batch=self.fixed_batch, group=self.background, x = x_pos, y = y_pos_up))
+        self.white_active_squares.append(((self.window_rows-1) * self.window_cols)+x+1)
+        self.white_active_squares_position.append([x_pos,y_pos_up])
         x_pos += self.img_pix
+    print ("Active white squares", self.white_active_squares)
+    print ("Active white square positions", self.white_active_squares_position)
 
     # draw vertical green controls
     y_pos = 0
@@ -45,8 +57,11 @@ def DrawBordersAndBackgrounds (self):
     #   their own list.
     widget_list = list(self.widget_pile_list)
 
+    self.widget_active_squares = []
+    self.widget_active_squares_position = []
     y_pos = self.img_pix * (self.game_rows+1)
-    for x in range(self.game_rows+1):
+    win_pos = self.window_squares - self.control_cols
+    for x in range((self.game_rows+2)//2):
         x_pos = self.img_pix * (self.game_cols+3)
         for y in range(self.control_cols):
             try:
@@ -56,12 +71,17 @@ def DrawBordersAndBackgrounds (self):
                 self.spr_mv_list[spr_index][2].x = x_pos
                 self.spr_mv_list[spr_index][2].y = y_pos
                 self.fixed_sprites.append( self.spr_mv_list[spr_index][2])
+                self.widget_active_squares.append(win_pos+y)
+                self.widget_active_squares_position.append([x_pos,y_pos])
             except:
                 self.fixed_sprites.append( pyglet.sprite.Sprite( self.gray_bg_image, batch=self.fixed_batch, group=self.background, x = x_pos, y = y_pos))
             x_pos += self.img_pix
             if spr_index < 33:
                 spr_index += 1
         y_pos -= self.img_pix*2
+        win_pos -= self.window_cols*2
+    print ("Active widget squares", self.widget_active_squares)
+    print ("Active widget square positions", self.widget_active_squares_position)
 
     # then fill in the rest of the rows with the 
     # gray background.
