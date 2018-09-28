3ef8baf75157f8bdd9d6af51573726591f97dc1d
ant <ant@anthive.com>
2018-09-28 02:24:06 -0400

    putting down guesses

--- active.py
+++ active.py
@@ -19,14 +19,14 @@ def MarbleInMotion (self, x_rec, y_rec, win_pos):
     pass
 
 
-def DoWhiteAction (self, x_rec, y_rec, win_pos):
+def DoWhiteAction (self, x, x_rec, y, y_rec, win_pos):
 
     ShowWhiteInArrow (self, x_rec, y_rec, win_pos)
     MarbleInMotion (self, x_rec, y_rec, win_pos)
     ShowWhiteOutArrow (self, x_rec, y_rec, win_pos)
 
 
-def DoWidgetAction (self, x_rec, y_rec, win_pos):
+def DoWidgetAction (self, x, x_rec, y, y_rec, win_pos):
 
     widget_index = self.widget_active_squares.index(win_pos)
     widget_spr_index = self.widget_pile_list[widget_index]
@@ -39,6 +39,8 @@ def DoWidgetAction (self, x_rec, y_rec, win_pos):
         if (win_pos == self.picked_up_window_square):
             self.picked_up_window_square = -1
             self.picked_up = False
+            self.picked_up_widget = 0
+            self.picked_up_widget_index = 0
             self.picked_up_sprite.visible = False
             self.picked_up_sprite.image = self.white_bg_image
             self.picked_up_sprite_snap_back = 0
@@ -48,45 +50,65 @@ def DoWidgetAction (self, x_rec, y_rec, win_pos):
         else:
             self.picked_up_window_square = win_pos
             self.picked_up_sprite.image = self.spr_mv_list[widget_spr_index][1][0]
-            self.picked_up_sprite.x = x_rec
-            self.picked_up_sprite.y = y_rec
+            self.picked_up_sprite.x = x - self.half_img_pix - 4
+            self.picked_up_sprite.y = y - self.half_img_pix - 4
             self.picked_up_sprite_snap_back = x_rec
             self.picked_up_sprite_snap_back = y_rec
             self.picked_up_sprite.visible = True
+            self.picked_up_widget = widget_spr_index
+            self.picked_up_widget_index = widget_index
             print ("Picked up other item ", widget_index)
             self.cube.visible = False
     else:
         self.picked_up_window_square = win_pos
         self.picked_up = True
         self.picked_up_sprite.image = self.spr_mv_list[widget_spr_index][1][0]
-        self.picked_up_sprite.x = x_rec
-        self.picked_up_sprite.y = y_rec
+        self.picked_up_sprite.x = x - self.half_img_pix - 4
+        self.picked_up_sprite.y = y - self.half_img_pix - 4
         self.picked_up_sprite_snap_back = x_rec
         self.picked_up_sprite_snap_back = y_rec
         self.picked_up_sprite.visible = True
+        self.picked_up_widget = widget_spr_index
+        self.picked_up_widget_index = widget_index
         print ("Picked up ", widget_index)
         self.cube.visible = False
 
 
-def DoGuessAction (self, x_rec, y_rec, win_pos):
+def DoGuessAction (self, x, x_rec, y, y_rec, win_pos):
 
-    print ("Waiting for a valid click on game board square")
-
-
-def ActiveAreaAction (self, x_rec, y_rec, win_pos):
-
-    this.square = win_pos
-    if (win_pos in self.white_active_squares):
-        print ("selected ", win_pos, " which is an active White square.")
-        DoWhiteAction (self, x_rec, y_rec, win_pos)
-    elif (win_pos in self.widget_active_squares):
-        print ("selected ", win_pos, " which is an active Widget square.")
-        DoWidgetAction (self, x_rec, y_rec, win_pos)
-    elif (win_pos in self.guess_active_squares):
-        print ("selected ", win_pos, " which is an active Guess square.")
-        DoGuessAction (self, x_rec, y_rec, win_pos)
+    if (self.picked_up == True):
+        guess_board_index = self.board_to_window_index.index(win_pos)
+        if (self.board[guess_board_index][1] != 0):
+            print ("Guess board square not empty...")
+        else:
+            guess_index = self.guess_active_squares.index(win_pos)
+            print ("guess_index ", guess_index)
+            print ("picked_up_widget ", self.picked_up_widget)
+            self.guess_sprites[guess_index].image = self.spr_mv_list[self.picked_up_widget][1][0]
+            self.board[guess_index][1] = self.picked_up_widget
+            self.widget_pile_list_counts[self.picked_up_widget_index] -= 1
+            self.picked_up = False
+            self.picked_up_sprite.visible = False
+            print ("Put it there")
     else:
         pass
+
+
+def ActiveAreaAction (self, x, x_rec, y, y_rec, win_pos):
+
+    this.square = win_pos
+    if (self.show_board == 1):
+        if (win_pos in self.white_active_squares):
+            print ("selected ", win_pos, " which is an active White square.")
+            DoWhiteAction (self, x, x_rec, y, y_rec, win_pos)
+        elif (win_pos in self.widget_active_squares):
+            print ("selected ", win_pos, " which is an active Widget square.")
+            DoWidgetAction (self, x, x_rec, y, y_rec, win_pos)
+        elif (win_pos in self.guess_active_squares):
+            print ("selected ", win_pos, " which is an active Guess square.")
+            DoGuessAction (self, x, x_rec, y, y_rec, win_pos)
+        else:
+            pass
 
 
 def ActiveAreaMouseAction (self, x, x_rec, y, y_rec, win_pos):
@@ -96,15 +118,15 @@ def ActiveAreaMouseAction (self, x, x_rec, y, y_rec, win_pos):
             if (this.square != win_pos):
                 print ("moved over ", win_pos, " which is an active Widget square.")
                 this.square = win_pos
-            self.picked_up_sprite.x = x_rec
-            self.picked_up_sprite.y = y_rec
+            self.picked_up_sprite.x = x - self.half_img_pix - 4
+            self.picked_up_sprite.y = y - self.half_img_pix - 4
             self.picked_up_sprite.visible = True
         elif (win_pos in self.guess_active_squares):
             if (this.square != win_pos):
                 print ("moved over ", win_pos, " which is an active Guess square.")
                 this.square = win_pos
-            self.picked_up_sprite.x = x_rec
-            self.picked_up_sprite.y = y_rec
+            self.picked_up_sprite.x = x - self.half_img_pix - 4
+            self.picked_up_sprite.y = y - self.half_img_pix - 4
             self.picked_up_sprite.visible = True
             self.possible_place_guess_x = x_rec
             self.possible_place_guess_y = y_rec
--- labels.py
+++ labels.py
@@ -1,30 +1,33 @@
 import pyglet
 import copy
+from time import sleep
 
 
 def AddLabels (self):
 
-    self.widget_count_list = copy.deepcopy(self.widget_pile_list_counts)
-
-    y_pos = self.img_pix * (self.game_rows)
-    for i in range(self.game_rows+1):
-        x_pos = self.img_pix * (self.game_cols+3)
-        for j in range(self.control_cols):
-            if ((i % 2) == 0):
-                try:
-                    count_str = str(self.widget_count_list.pop(0))
-                    self.score = pyglet.text.Label(
-                        count_str,
-                        font_name='Sans Regular',
-                        font_size=28,
-                        bold=True,
-                        color=[245, 0, 0, 255],
-                        width=self.img_pix, height=self.img_pix,
-                        x=x_pos+34, y=y_pos+24,
-                        anchor_x='center', anchor_y='center',
-                        batch=self.text_batch)
-                except:
-                    break
-            x_pos += self.img_pix
-        y_pos -= self.img_pix
-
+    if (len(self.widget_labels) == 0):
+        self.widget_count_list = copy.deepcopy(self.widget_pile_list_counts)
+        y_pos = self.img_pix * (self.game_rows)
+        for i in range(self.game_rows+1):
+            x_pos = self.img_pix * (self.game_cols+3)
+            for j in range(self.control_cols):
+                if ((i % 2) == 0):
+                    try:
+                        count_str = str(self.widget_count_list.pop(0))
+                        self.widget_labels.append(pyglet.text.Label(
+                            count_str,
+                            font_name='Sans Regular',
+                            font_size=28,
+                            bold=True,
+                            color=[245, 0, 0, 255],
+                            width=self.img_pix, height=self.img_pix,
+                            x=x_pos+34, y=y_pos+24,
+                            anchor_x='center', anchor_y='center',
+                            batch=self.text_batch))
+                    except:
+                        break
+                x_pos += self.img_pix
+            y_pos -= self.img_pix
+    else:
+        del self.widget_labels
+        self.widget_labels = []
